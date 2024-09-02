36102149630a9e87633aa4dfd646ec5c0e73d6b3
李九天 <14749974+Swedream-Li@user.noreply.gitee.com>
2024-09-02 18:12:45 +0800

    test

---
+++ build/CMakeFiles/git_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_demo.dir/git_demo.cpp.o"
+  "git_demo"
+  "git_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
